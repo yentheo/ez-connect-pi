953e57f4a2f881c53e5ab9410312b43f5b9fa77b
Yenthe Oerlemans <yentheoerlemans@gmail.com>
2020-01-07 00:11:00 +0100

    remove prints

--- wifi_utils.py
+++ wifi_utils.py
@@ -13,29 +13,20 @@ def get_current_connected_wifi():
         return None
 
 def connect_to_wifi(ssid, passkey):
-    print('connection to wifi ' + ssid)
-
     p = Popen(['sudo', '/sbin/wpa_cli'], stdout=PIPE, stdin=PIPE, stderr=PIPE)
-    stdout_data = p.communicate(input=('set_network 0 ssid "' + ssid  + '"').encode())[0]
-    print(stdout_data)
+    p.communicate(input=('set_network 0 ssid "' + ssid  + '"').encode())
     p = Popen(['sudo', '/sbin/wpa_cli'], stdout=PIPE, stdin=PIPE, stderr=PIPE)
-    stdout_data = p.communicate(input=('set_network 0 psk "' + passkey + '"').encode())[0]
-    print(stdout_data)
+    p.communicate(input=('set_network 0 psk "' + passkey + '"').encode())
     p = Popen(['sudo', '/sbin/wpa_cli'], stdout=PIPE, stdin=PIPE, stderr=PIPE)
-    stdout_data = p.communicate(input='enable_network 0'.encode())[0]
-    print(stdout_data)
+    p.communicate(input='enable_network 0'.encode())
     p = Popen(['sudo', '/sbin/wpa_cli'], stdout=PIPE, stdin=PIPE, stderr=PIPE)
-    stdout_data = p.communicate(input='save_config'.encode())[0]
-    print(stdout_data)
+    p.communicate(input='save_config'.encode())
 
     f = open('/etc/wpa_supplicant/wpa_supplicant.conf', 'r')
     wpa_supplicant = f.read()
-    print(wpa_supplicant)
 
     country_regex = "^country=[A-Z]{2}$"
     country_code_absent = re.match(country_regex, wpa_supplicant, re.MULTILINE) is None
-
-    print(country_code_absent)
 
     if country_code_absent:
         with open('/etc/wpa_supplicant/wpa_supplicant.conf', 'w') as modified: modified.write("country=BE\n" + wpa_supplicant)
